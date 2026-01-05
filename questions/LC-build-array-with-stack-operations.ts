c3df887188ecb0e77dc6d07095f8047d9abd8305
codingbbq <codingbbq@gmail.com>
2026-01-05 23:35:55 +0530

    LC - Stack

---
+++ questions/LC-build-array-with-stack-operations.ts
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/build-an-array-with-stack-operations/description/?envType=problem-list-v2&envId=dsa-linear-shoal-stack
+
+(() => {
+	function buildArray(target: number[], n: number): string[] {}
+
+	console.log(buildArray([1, 3], 3)); // ["Push","Push","Pop","Push"]
+})();
